b3d14ccbaa2e707dda4d26e805a7f56f7821c3ad
cbretheim <33535137+cbretheim@users.noreply.github.com>
2019-03-03 21:09:41 +0100

    Add files via upload

---
+++ wk6/script.js
@@ -0,0 +1,106 @@
+//For readability and clarity it is good practice to declare variables at the beginning of the JS document if possible
+var churchillSpeech = {
+      'author': 'Churchill',
+      'year': 1940,
+      'yearIsBCE': false,
+      'authorAge': '66'
+    },
+    ghandiSpeech = {
+      'author': 'Ghandi',
+      'year': 1942,
+      'yearIsBCE': false,
+      'authorAge': '73'
+    },
+    demosthenesSpeech = {
+      'author': 'Demosthenes',
+      'year': 342,
+      'yearIsBCE': true,
+      'authorAge': '42'
+    },
+    speechesArray = [churchillSpeech, ghandiSpeech, demosthenesSpeech],
+    userNamePrompt,
+    favoriteSpeechPrompt;
+
+var i=0;
+
+var oldestSpeech=speechesArray[0].year;
+var newestSpeech=speechesArray[0].year;
+
+for (i=0; i<speechesArray.length; i++) {
+  if(speechesArray[i].year <= oldestSpeech) {
+  oldestSpeech=speechesArray[i].year;
+}
+}
+
+for (i=0; i<speechesArray.length; i++) {
+  if(speechesArray[i].year >= newestSpeech) {
+  newestSpeech=speechesArray[i].year;
+}}
+
+while(i<speechesArray.length) {
+  console.log("This speech is written by " + speechesArray[i].author);
+  i++;
+}
+
+document.getElementById('BtnDonate').addEventListener('click', function(){
+  //Code in here executes when the user clicks the "Donate" button.
+  favoriteSpeechPrompt = window.prompt('Which speech author is your favorite?');
+
+  for (i=0; i<speechesArray.length; i++) {
+    console.log(speechesArray[i].author + " was " + speechesArray[i].authorAge + " during this speech.");
+  }
+})
+
+
+document.getElementById('BtnChurchill').addEventListener('click', function(){
+  //Code in here executes when the user clicks the "Churchill" button.
+  console.log('This speech was written by ' + speechesArray[0].author + ' in ' + speechesArray[0].year);
+
+if(speechesArray[0].year<=oldestSpeech) {
+  console.log('This is the oldest speech on the page.')
+} else if(speechesArray[0].year>=newestSpeech) {
+  console.log('This is the most recent speech on the page.');
+} else {
+  console.log('This is neither the newest nor oldest speech on the page.');
+}
+});
+
+
+document.getElementById('BtnGhandi').addEventListener('click', function(){
+  //Code in here executes when the user clicks the "Ghandi" button.
+  console.log('This speech was written by ' + speechesArray[1].author + ' in ' + speechesArray[1].year);
+
+
+if(speechesArray[1].year<=oldestSpeech) {
+  console.log('This is the oldest speech on the page.')
+} else if(speechesArray[1].year>=newestSpeech) {
+  console.log('This is the most recent speech on the page.');
+} else {
+  console.log('This is neither the newest nor oldest speech on the page.');
+}
+});
+
+document.getElementById('BtnDemosthenes').addEventListener('click', function(){
+  //Code in here executes when the user clicks the "Demosthenes" button.
+  console.log('This speech was written by ' + speechesArray[2].author + ' in ' + speechesArray[2].year);
+
+
+if(speechesArray[2].year<=oldestSpeech) {
+  console.log('This is the oldest speech on the page.')
+} else if(speechesArray[2].year>=newestSpeech) {
+  console.log('This is the most recent speech on the page.');
+} else {
+  console.log('This is neither the newest nor oldest speech on the page.');
+}
+});
+
+
+
+userNamePrompt = window.prompt('Hello, what is your name?');
+
+if(typeof userNamePrompt === 'string' && userNamePrompt !== ''){
+  console.log('Hi ' + userNamePrompt + '!');
+}else{
+  console.log('Ok, I\'ll just call you User.');
+  userNamePrompt = 'User';
+}
